e8fa49087d9d63e6c1c0a1b3d8fd4312e844dff4
threetown <threetowns@163.com>
2017-06-26 14:04:32 +0800

    Add validDrag animation.

--- js/ValidDrag.js
+++ js/ValidDrag.js
@@ -11,7 +11,7 @@
         var options = $.extend(defaults, options);
         //添加背景，文字，滑块
         var html = '<div class="drag_bg"></div>'+
-                    '<div class="drag_text" onselectstart="return false;" unselectable="on">拖动滑块验证</div>'+
+                    '<div class="drag_text" onselectstart="return false;" unselectable="on">请按住滑块，拖动到最右边</div>'+
                     '<div class="handler handler_bg"></div>';
         this.append(html);
         
